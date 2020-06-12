a804b78d839f8697391769173963398cbd775a38
alexcercos <44820718+alexcercos@users.noreply.github.com>
2020-06-13 00:01:40 +0200

    spanwer

--- UnitySDK/Assets/ML-Agents/Examples/DemoShot/Scripts/RenderTController.cs
+++ UnitySDK/Assets/ML-Agents/Examples/DemoShot/Scripts/RenderTController.cs
@@ -1,30 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class RenderTController : MonoBehaviour
 {
-    public Texture2D defaultTexture;
+    [Serializable]
+    public class PreviousFrame
+    {
+        PreviousFrame()
+        {
+            position = 0;
+            frame = null;
+        }
+        public int position;
+        public RenderTexture frame;
+    }
+
+    //public Texture2D defaultTexture;
 
     public RenderTexture current;
-    public List<RenderTexture> previousFrames;
+    public List<RenderTexture> renderQueue;
+
+    int initFrameCount = 0;
+
+    //Se da por hecho que este ordenado
+    public List<PreviousFrame> previousFrames;
+    
+    int maxIndex;
 
     public bool saveImg = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        renderQueue = new List<RenderTexture>();
 
-        for (int i = 0; i < previousFrames.Count; i++)
-        {
-            if (previousFrames[i]==null)
-            {
-                previousFrames[i] = new RenderTexture(current.width, current.height, current.depth);
-
-                Graphics.Blit(defaultTexture, previousFrames[i]);
-            }
-        }
-
+        if (previousFrames.Count > 0)
+            maxIndex = previousFrames[previousFrames.Count - 1].position;
     }
 
     private void LateUpdate()
@@ -35,13 +49,50 @@ public class RenderTController : MonoBehaviour
             saveImg = false;
         }
 
-        for (int i = previousFrames.Count - 1; i > 0; i--)
+        if (initFrameCount > 2) //frames necesarios para que empiece a guardar rts
         {
-            Graphics.Blit(previousFrames[i - 1], previousFrames[i]);
+            RenderTexture newRT = new RenderTexture(current.width, current.height, current.depth);
+            Graphics.Blit(current, newRT);
+
+            renderQueue.Insert(0, newRT);
+        }
+        else
+        {
+            initFrameCount++;
+        }
+        
+
+        //Actualizar texturas para el siguiente frame
+
+        for (int i = 0; i < previousFrames.Count; i++)
+        {
+            if (previousFrames[i].position < renderQueue.Count)
+            {
+                Graphics.Blit(renderQueue[previousFrames[i].position], previousFrames[i].frame);
+            }
+            else if (renderQueue.Count > 0)
+            {
+                Graphics.Blit(renderQueue[renderQueue.Count-1], previousFrames[i].frame);
+            }
+            else
+            {
+                Graphics.Blit(current, previousFrames[i].frame);
+            }
         }
 
-        Graphics.Blit(current, previousFrames[0]);
-        
+        if (renderQueue.Count > maxIndex + 1)
+        {
+            Destroy(renderQueue[renderQueue.Count-1]);
+            renderQueue.RemoveAt(renderQueue.Count - 1);
+
+            //GC.Collect();
+        }
+
+
+
+        //Graphics.Blit(current, previousFrames[0]);
+
+        //Debug.Log(renderQueue.Count);
     }
 
 
---
+++ UnitySDK/Assets/ML-Agents/Examples/DemoShot/Scripts/SpawnCases.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCases : MonoBehaviour
+{
+    public GameObject plane;
+
+    public Transform cameraYAxis;
+
+    public float timeBetweenSpawn = 2.0f;
+    float timeElapsed = 0f;
+
+    public float minDist = 30f;
+    public float maxDist = 400f;
+
+    float cameraFOV = 60f; //FOV horizontal, comprobado a mano (en realidad es 63.7 pero se desprecia la diferencia)
+    
+
+    [Range(0.0f, 1.0f)]
+    public float commonCase;
+    [Range(0.0f, 1.0f)]
+    public float inLeftCase;
+    [Range(0.0f, 1.0f)]
+    public float inRightCase;
+    [Range(0.0f, 1.0f)]
+    public float consecutiveCase;
+
+
+    private void OnValidate()
+    {
+        
+        float total = commonCase + inLeftCase + inRightCase + consecutiveCase;
+        
+        if (total == 0f)
+        {
+            commonCase = inLeftCase = inRightCase = consecutiveCase = 1 / 4f;
+        }
+        else
+        {
+            commonCase /= total;
+            inLeftCase /= total;
+            inRightCase /= total;
+            consecutiveCase /= total;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+
+        if (timeElapsed > timeBetweenSpawn)
+        {
+            float rand = Random.Range(0f, 1f);
+
+            //Debug.Log(rand);
+            if (rand < commonCase)
+                SpawnPlane(ESpawnCase.OUTLEFT);
+            rand -= commonCase;
+
+            if (rand>=0f  && rand < inLeftCase)
+                SpawnPlane(ESpawnCase.INLEFT);
+            
+            rand -= inLeftCase;
+
+            if (rand >= 0f && rand < inRightCase)
+                SpawnPlane(ESpawnCase.INRIGHT);
+
+            rand -= inLeftCase;
+
+            if (rand >= 0f && rand < consecutiveCase)
+                SpawnPlane(ESpawnCase.CONSECUTIVE);
+
+            //rand -= consecutiveCase;
+        }
+    }
+
+    void SpawnPlane(ESpawnCase typeSpawn)
+    {
+        timeElapsed = 0f;
+
+        //Debug.Log(typeSpawn);
+
+        float dist = Random.Range(minDist, maxDist);
+        float angle = 0f;
+
+        float left = (cameraYAxis.rotation.eulerAngles.y - cameraFOV / 2f);
+        float right = (cameraYAxis.rotation.eulerAngles.y + cameraFOV / 2f);
+
+        if (typeSpawn == ESpawnCase.INLEFT)
+        {
+            angle = Random.Range(left, cameraYAxis.rotation.eulerAngles.y); //por dentro
+        }
+        else if (typeSpawn == ESpawnCase.INRIGHT)
+        {
+            angle = Random.Range(cameraYAxis.rotation.eulerAngles.y, right); //por dentro
+        }
+        else
+        {
+            //if (typeSpawn == ESpawnCase.CONSECUTIVE)
+            angle = Random.Range(left - 10f, left - 30f); //por fuera
+
+            GameObject newPlaneExtra = Instantiate(plane, new Vector3(0f, 0f, dist), transform.rotation, transform);
+            newPlaneExtra.transform.RotateAround(transform.position, Vector3.up, angle);
+
+            angle = angle - Random.Range(0f, cameraFOV / 2f); //Vuelve a spawnear consecutivo
+            dist = Random.Range(minDist, maxDist);
+
+            if (typeSpawn == ESpawnCase.OUTLEFT)
+                return;
+        }
+        
+
+
+        GameObject newPlane = Instantiate(plane, new Vector3(0f, 0f, dist), transform.rotation, transform);
+        newPlane.transform.RotateAround(transform.position, Vector3.up, angle);
+
+    }
+}
+
+public enum ESpawnCase
+{
+    OUTLEFT, INLEFT, INRIGHT, CONSECUTIVE
+}
